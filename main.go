c63b38f230edeb9b5fa7faea6901b360b966b4af
Anaminus <anaminus@protonmail.com>
2017-05-29 10:32:27 -0500

    Move registration to separate file.

--- main.go
+++ main.go
@@ -22,53 +22,6 @@ type Source struct {
 	Values     []rbxfile.Value
 }
 
-// Receives a Node and a reference string. ref is n.Reference after it has been
-// parsed by the protocol detector, and excludes the scheme ("scheme://")
-// portion of the string, if it was given.
-type InputSchemeHandler func(opt *Options, node *InputNode, ref string) (src *Source, err error)
-
-var registeredInputSchemes = map[string]InputSchemeHandler{}
-
-func RegisterInputScheme(name string, handler InputSchemeHandler) {
-	if handler == nil {
-		panic("cannot register nil scheme handler")
-	}
-	if _, registered := registeredInputSchemes[name]; registered {
-		panic("scheme already registered")
-	}
-	registeredInputSchemes[name] = handler
-}
-
-// Receives a Node and a reference string. ref is n.Reference after it has been
-// parsed by the protocol detector, and excludes the scheme ("scheme://")
-// portion of the string, if it was given. Also receives an input source.
-type OutputSchemeHandler func(opt *Options, node *OutputNode, ref string, src *Source) (err error)
-
-var registeredOutputSchemes = map[string]OutputSchemeHandler{}
-
-func RegisterOutputScheme(name string, handler OutputSchemeHandler) {
-	if handler == nil {
-		panic("cannot register nil scheme handler")
-	}
-	if _, registered := registeredOutputSchemes[name]; registered {
-		panic("scheme already registered")
-	}
-	registeredOutputSchemes[name] = handler
-}
-
-var registeredFilters = map[string]Filter{}
-
-func RegisterFilter(filter Filter) {
-	if filter == nil {
-		panic("cannot register nil scheme handler")
-	}
-	name, _ := filter.Signature()
-	if _, registered := registeredFilters[name]; registered {
-		panic("filter already registered")
-	}
-	registeredFilters[name] = filter
-}
-
 // parseScheme separates a string into scheme and path parts.
 func parseScheme(s string) (scheme, path string) {
 	for i := 0; i < len(s); i++ {
---
+++ registers.go
@@ -0,0 +1,48 @@
+package main
+
+// Receives a Node and a reference string. ref is n.Reference after it has been
+// parsed by the protocol detector, and excludes the scheme ("scheme://")
+// portion of the string, if it was given.
+type InputSchemeHandler func(opt *Options, node *InputNode, ref string) (src *Source, err error)
+
+var registeredInputSchemes = map[string]InputSchemeHandler{}
+
+func RegisterInputScheme(name string, handler InputSchemeHandler) {
+	if handler == nil {
+		panic("cannot register nil scheme handler")
+	}
+	if _, registered := registeredInputSchemes[name]; registered {
+		panic("scheme already registered")
+	}
+	registeredInputSchemes[name] = handler
+}
+
+// Receives a Node and a reference string. ref is n.Reference after it has been
+// parsed by the protocol detector, and excludes the scheme ("scheme://")
+// portion of the string, if it was given. Also receives an input source.
+type OutputSchemeHandler func(opt *Options, node *OutputNode, ref string, src *Source) (err error)
+
+var registeredOutputSchemes = map[string]OutputSchemeHandler{}
+
+func RegisterOutputScheme(name string, handler OutputSchemeHandler) {
+	if handler == nil {
+		panic("cannot register nil scheme handler")
+	}
+	if _, registered := registeredOutputSchemes[name]; registered {
+		panic("scheme already registered")
+	}
+	registeredOutputSchemes[name] = handler
+}
+
+var registeredFilters = map[string]Filter{}
+
+func RegisterFilter(filter Filter) {
+	if filter == nil {
+		panic("cannot register nil scheme handler")
+	}
+	name, _ := filter.Signature()
+	if _, registered := registeredFilters[name]; registered {
+		panic("filter already registered")
+	}
+	registeredFilters[name] = filter
+}
