2d6b09f0131fefc55ba49fa14134b4df27d5ee8b
Anaminus <anaminus@protonmail.com>
2017-10-14 17:09:50 -0500

    Revert "minify.lua: Fix vararg operator not being parsed in function arguments."
    This reverts commit 0d3e86171c983f2952027a231aaf26287eebab77.

--- filter/minify.lua
+++ filter/minify.lua
@@ -633,7 +633,7 @@ function CreateLuaParser(text)
 	end
 
 	-- List of identifiers
-	local function varlist(funcArgs)
+	local function varlist()
 		local varList = {}
 		local commaList = {}
 		if peek().Type == 'Ident' then
@@ -641,11 +641,6 @@ function CreateLuaParser(text)
 		end
 		while peek().Source == ',' do
 			table.insert(commaList, get())
-			if funcArgs and peek().Source == '...' then
-				local id = get()
-				table.insert(varList, id)
-				break
-			end
 			local id = expect('Ident')
 			table.insert(varList, id)
 		end
@@ -689,7 +684,7 @@ function CreateLuaParser(text)
 		end
 		--
 		local oparenTk = expect('Symbol', '(')
-		local argList, argCommaList = varlist(true)
+		local argList, argCommaList = varlist()
 		local cparenTk = expect('Symbol', ')')
 		local fbody, enTk = blockbody('end')
 		--
