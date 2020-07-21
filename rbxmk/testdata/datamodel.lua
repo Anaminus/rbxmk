9cdfb1b3f06bae99c5ba19bd6def0373c4406d7a
Anaminus <anaminus@protonmail.com>
2020-07-21 09:14:00 -0500

    Expand tests.

--- rbxmk/testdata/datamodel.lua
+++ rbxmk/testdata/datamodel.lua
@@ -1,5 +1,7 @@
 local game = DataModel.new()
-game.ExplicitAutoJoints = true
+assert(pcall(function() game.ExplicitAutoJoints = true end), "DataModel must set properties")
+assert(game.ExplicitAutoJoints == true, "DataModel must get properties")
+assert(not pcall(function() game.ClassName = "BoolValue" end), "cannot set ClassName of DataModel")
 
 local workspace = game:GetService("Workspace")
 assert(workspace.ClassName == "Workspace", "GetService must set ClassName to given value")
@@ -7,4 +9,4 @@ assert(workspace.Name == "Workspace", "GetService must set Name to given value")
 assert(workspace.Parent == game, "GetService must set Parent to DataModel")
 assert(game:GetService("Workspace") == workspace, "GetService must return singleton")
 
-assert(not pcall(function() Instance.new("BoolValue"):GetService("Workspace") end))
+assert(not pcall(function() Instance.new("BoolValue"):GetService("Workspace") end), "non-DataModel instance must not have GetService")
