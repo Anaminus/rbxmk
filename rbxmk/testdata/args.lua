e460369a7b0490195ab9ad44f9d8b2eefbb1f1f1
Anaminus <anaminus@protonmail.com>
2020-08-08 11:16:08 -0500

    Use new format for argument test.

--- rbxmk/testdata/args.lua
+++ rbxmk/testdata/args.lua
@@ -1,9 +1,17 @@
 -- Test arguments
-assert(select(1, ...) == true, "arg 1 must be true")
-assert(select(2, ...) == false, "arg 2 must be false")
-assert(select(3, ...) == nil, "arg 3 must be nil")
-assert(select(4, ...) == 42, "arg 4 must be 42")
-assert(select(5, ...) == 3.141592653589793, "arg 5 must be pi")
-assert(select(6, ...) == -1e-8, "arg 6 must be -1e-8")
-assert(select(7, ...) == "hello, world!", "arg 7 must be string")
-assert(select(8, ...) == "hello\0world!", "arg 8 must be string with NUL")
+T.Pass("arg 1 is true",
+	select(1, ...) == true)
+T.Pass("arg 2 is false",
+	select(2, ...) == false)
+T.Pass("arg 3 is nil",
+	select(3, ...) == nil)
+T.Pass("arg 4 is 42",
+	select(4, ...) == 42)
+T.Pass("arg 5 is pi",
+	select(5, ...) == 3.141592653589793)
+T.Pass("arg 6 is -1e-8",
+	select(6, ...) == -1e-8)
+T.Pass("arg 7 is string",
+	select(7, ...) == "hello, world!")
+T.Pass("arg 8 is string with NUL",
+	select(8, ...) == "hello\0world!")
