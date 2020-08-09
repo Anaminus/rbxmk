adcb94e326182b025b736568617af9aae5962c41
Anaminus <anaminus@protonmail.com>
2020-08-09 12:24:06 -0500

    Test for relative userdata cache length.

--- rbxmk/testdata/userdata.lua
+++ rbxmk/testdata/userdata.lua
@@ -1,8 +1,9 @@
 T.GC()
+local baseLen = T.UserDataCacheLen()
 
 local game = DataModel.new()
 local object0 = Instance.new("Object", game)
-T.Pass("userdata cache length is 2", T.UserDataCacheLen() == 2)
+T.Pass("userdata cache length is 2", T.UserDataCacheLen() == baseLen+2)
 
 local set = {[object0] = true}
 local object1 = game:GetChildren()[1]
@@ -10,16 +11,16 @@ T.Pass("cached userdata is returned", set[object1] == true)
 
 game = nil
 T.GC()
-T.Pass("userdata cache length is 1", T.UserDataCacheLen() == 1)
+T.Pass("userdata cache length is 1", T.UserDataCacheLen() == baseLen+1)
 
 object0 = nil
 T.GC()
-T.Pass("userdata cache length is 1", T.UserDataCacheLen() == 1)
+T.Pass("userdata cache length is 1", T.UserDataCacheLen() == baseLen+1)
 
 object1 = nil
 T.GC()
-T.Pass("userdata cache length is 1", T.UserDataCacheLen() == 1)
+T.Pass("userdata cache length is 1", T.UserDataCacheLen() == baseLen+1)
 
 set = nil
 T.GC()
-T.Pass("userdata cache length is 0", T.UserDataCacheLen() == 0)
+T.Pass("userdata cache length is 0", T.UserDataCacheLen() == baseLen+0)
