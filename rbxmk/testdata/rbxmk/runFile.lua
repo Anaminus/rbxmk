7df394d819de0e890bb1632bda70b89fddc6b3b6
Anaminus <anaminus@protonmail.com>
2020-08-09 20:23:04 -0500

    Test type of first argument to rbxmk.runFile.

--- rbxmk/testdata/rbxmk/runFile.lua
+++ rbxmk/testdata/rbxmk/runFile.lua
@@ -1,4 +1,6 @@
 local average = os.join(os.expand("$sd"),"average.lua")
+T.Fail("runFile expects string for its first argument",
+	function() rbxmk.runFile(42) end)
 T.Pass("runFile receives arguments and returns results",
 	rbxmk.runFile(average,1,2,3,4,5,6) == 3.5)
 T.Fail("runFile throws error when script errors",
