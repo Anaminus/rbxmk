a7913ddd9ddd600609f8ce3bb88c6569250ddb7e
Anaminus <anaminus@protonmail.com>
2020-08-10 13:23:44 -0500

    Rename average.lua and badsyntax.lua.

--- rbxmk/testdata/rbxmk/runFile.lua
+++ rbxmk/testdata/rbxmk/runFile.lua
@@ -3,10 +3,10 @@ T.Fail("runFile expects string for its first argument",
 T.Fail("runFile throws an error when file does not exist",
 	function() rbxmk.runFile("nonextant.lua") end)
 T.Pass("runFile receives arguments and returns results",
-	rbxmk.runFile(os.expand("$sd/average.lua"),1,2,3,4,5,6) == 3.5)
+	rbxmk.runFile(os.expand("$sd/_average.lua"),1,2,3,4,5,6) == 3.5)
 T.Fail("runFile throws an error when script errors",
-	function() rbxmk.runFile(os.expand("$sd/average.lua"),1,2,3,true,5,6) end)
+	function() rbxmk.runFile(os.expand("$sd/_average.lua"),1,2,3,true,5,6) end)
 T.Fail("runFile throws an error when script is already running",
 	function() rbxmk.runFile(os.expand("$sd/$sn")) end)
 T.Fail("runFile throws an error when script could not be loaded",
-	function() rbxmk.runFile(os.expand("$sd/badsyntax.lua")) end)
+	function() rbxmk.runFile(os.expand("$sd/_badsyntax.lua")) end)
