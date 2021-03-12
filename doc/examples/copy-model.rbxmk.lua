2070bf8d96a2b16d4e95f477f16c3d3a52048948
Anaminus <anaminus@protonmail.com>
2021-03-11 20:38:20 -0600

    Add examples.
    - Downloading an asset to a file.
    - Copy the content of a model to the clipboard.

---
+++ doc/examples/copy-model.rbxmk.lua
@@ -0,0 +1,24 @@
+-- Read a model asset URL from the clipboard, then copy the content of the model
+-- to the clipboard, ready to be pasted into Studio.
+--
+-- Usage:
+--     rbxmk run copy-model.rbxmk.lua
+--
+-- Use studio cookies:
+--     rbxmk run copy-model.rbxmk.lua true
+
+local useStudioCookies = ...
+local url = clipboard.read("txt")
+local id = tonumber(url:match("%d+"))
+if not id then
+	error(string.format("failed to parse asset ID from URL %q", url), 0)
+end
+local options = {
+	AssetID = id,
+	Format = "rbxm",
+}
+if useStudioCookies then
+	options.Cookies = rbxmk.cookiesFrom("studio")
+end
+local asset = rbxassetid.read(options)
+clipboard.write(asset, "rbxm")
---
+++ doc/examples/download-asset.rbxmk.lua
@@ -0,0 +1,10 @@
+-- Download a Roblox asset to a local file.
+--
+-- e.g. rbxmk run download-asset.rbxmk.lua 1818 Crossroads.rbxl
+
+local id, output = ...
+local asset = rbxassetid.read({
+	AssetID = id,
+	Format = "bin",
+})
+fs.write(output, asset, "bin")
