0e0a35b8efd94185e0d916f00abf0d998b7beccd
Anaminus <anaminus@protonmail.com>
2021-05-17 11:34:48 -0500

    Add fetch-transactions example.
    Uses economy API to download transactions made by a user.

---
+++ doc/examples/fetch-transactions.rbxmk.lua
@@ -0,0 +1,115 @@
+-- Output a CSV list of transactions for the user logged into Studio.
+--
+-- List possible transaction types:
+--     rbxmk run fetch-transactions.rbxmk.lua
+--
+-- List sales:
+--     rbxmk run fetch-transactions.rbxmk.lua sale
+
+local txnTypes = {
+	adspend               = true,
+	affiliatesale         = true,
+	csadjustment          = true,
+	currencypurchase      = true,
+	devex                 = true,
+	engagementpayout      = true,
+	groupengagementpayout = true,
+	grouppayout           = true,
+	individualtogroup     = true,
+	premiumstipend        = true,
+	purchase              = true,
+	sale                  = true,
+	traderobux            = true,
+}
+local txnType = string.lower(tostring((...) or ""))
+if not txnTypes[txnType] then
+	local s = {}
+	for k in pairs(txnTypes) do
+		table.insert(s, k)
+	end
+	table.sort(s)
+	print("first argument must be one of the following transaction types:")
+	for i, k in ipairs(s) do
+		print("\t"..k)
+	end
+	return
+end
+
+local cookies = rbxmk.cookiesFrom("studio")
+
+local CurrentUser do
+	local userURL = "https://www.roblox.com/mobileapi/userinfo"
+	local resp = http.request({
+		URL = userURL,
+		Method = "GET",
+		ResponseFormat = "json",
+		Cookies = cookies,
+	}):Resolve()
+	if not resp.Success then
+		print("failed to get user info:", resp.StatusMessage)
+		return
+	end
+	CurrentUser = {
+		ID = resp.Body.UserID,
+		Name = resp.Body.UserName,
+		Robux = resp.Body.RobuxBalance,
+		Thumbnail = resp.Body.ThumbnailUrl,
+		Premium = resp.Body.IsPremium,
+	}
+end
+
+local function getPage(next)
+	local txnURL = "https://economy.roblox.com/v2/users/%d/transactions?transactionType=%s&limit=100&cursor=%s"
+	local resp = http.request({
+		URL = string.format(txnURL, CurrentUser.ID, txnType, next or ""),
+		Method = "GET",
+		ResponseFormat = "json",
+		Cookies = cookies,
+	}):Resolve()
+	if not resp.Success then
+		return nil
+	end
+	local rows = {}
+	for i, entry in ipairs(resp.Body.data) do
+		local row = {
+			tostring(entry.id),
+			tostring(entry.transactionType),
+			tostring(entry.created),
+			tostring(entry.isPending),
+			tostring(entry.agent.id),
+			tostring(entry.agent.type),
+			tostring(entry.agent.name),
+			tostring(entry.details.id),
+			tostring(entry.details.name),
+			tostring(entry.details.type),
+			tostring(entry.currency.amount),
+			tostring(entry.currency.type),
+		}
+		table.insert(rows, row)
+	end
+	print(rbxmk.encodeFormat("csv", rows):sub(1,-2))
+	return resp.Body.nextPageCursor
+end
+
+local rows = {{
+	"id",
+	"transactionType",
+	"created",
+	"isPending",
+	"agent.id",
+	"agent.type",
+	"agent.name",
+	"details.id",
+	"details.name",
+	"details.type",
+	"currency.amount",
+	"currency.type",
+}}
+print(rbxmk.encodeFormat("csv", rows):sub(1,-2))
+local next
+repeat
+	next = getPage(next)
+	if not next then
+		break
+	end
+until not next
