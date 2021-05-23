b4e78054c02592fd8ba7e457c4146b5b2acb144a
Anaminus <anaminus@protonmail.com>
2021-05-23 12:34:17 -0500

    Add test to ensure fragments are compiled correctly.

---
+++ rbxmk/main_test.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"testing"
+)
+
+// TestFragments verifies that fragments have been compiled correctly.
+func TestFragments(t *testing.T) {
+	defer func() {
+		if v := recover(); v != nil {
+			t.Fatal(v)
+		}
+	}()
+	DocumentCommands()
+	UnresolvedFragments()
+}
