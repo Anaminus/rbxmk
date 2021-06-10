4a0b77f2dace582c9bae189346e4959a8a24697e
Anaminus <anaminus@protonmail.com>
2021-06-10 12:58:30 -0500

    Move World.Typeof to world_reflect.go.

--- world_reflect.go
+++ world_reflect.go
@@ -9,6 +9,24 @@ import (
 	"github.com/robloxapi/types"
 )
 
+// Typeof returns the type of the given Lua value. If it is a userdata, Typeof
+// attempts to get the type according to the value's metatable. Panics if v is
+// nil (not if nil Lua value).
+func (w *World) Typeof(v lua.LValue) string {
+	if v == nil {
+		panic("value expected")
+	}
+	u, ok := v.(*lua.LUserData)
+	if !ok {
+		return v.Type().String()
+	}
+	t, ok := w.l.GetMetaField(u, "__type").(lua.LString)
+	if !ok {
+		return u.Type().String()
+	}
+	return string(t)
+}
+
 // Push reflects v to lv.
 func (w *World) Push(v types.Value) (lv lua.LValue, err error) {
 	push, err := w.PusherOf(v.Type())
