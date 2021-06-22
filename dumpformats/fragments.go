767a40653a6d737e7866dde1dec6833388b98a51
Anaminus <anaminus@protonmail.com>
2021-06-22 08:08:05 -0500

    Extract fragment listing to separate function.

--- dumpformats/fragments.go
+++ dumpformats/fragments.go
@@ -15,156 +15,161 @@ var Fragments = Format{
 	Name: "fragments",
 	Func: func(w io.Writer, root dump.Root) error {
 		buf := bufio.NewWriter(w)
-		for _, library := range root.Libraries {
-			p := path.Join("libraries", library.Name)
-			fragWrite(buf, path.Join(p, "Summary"), library.Struct.Summary)
-			fragWrite(buf, path.Join(p, "Description"), library.Struct.Description)
-			fragWriteFields(buf, path.Join(p, "Fields"), library.Struct.Fields)
-			fragWriteTypes(buf, path.Join(p, "Types"), library.Types)
-		}
-		fragWriteTypes(buf, "types", root.Types)
-		fragWriteFormats(buf, "formats", root.Formats)
-		for _, frag := range root.Fragments {
-			fragWrite(buf, "fragments", frag)
-		}
+		ListFragments(root, func(path, ref string) {
+			switch ref {
+			case "":
+				buf.WriteString(">>>> ")
+				buf.WriteString(path)
+			case "$TODO":
+				buf.WriteString("---- ")
+				buf.WriteString(path)
+			default:
+				buf.WriteString(ref)
+			}
+			buf.WriteString("\n")
+		})
 		return buf.Flush()
 	},
 }
 
-func fragWrite(buf *bufio.Writer, p string, path string) {
-	switch path {
-	case "":
-		buf.WriteString(">>>> ")
-		buf.WriteString(p)
-	case "$TODO":
-		buf.WriteString("---- ")
-		buf.WriteString(p)
-	default:
-		buf.WriteString(path)
+// ListFragments traverses root, calling write for each fragment reference that
+// was found. ref is the fragment reference, and path describes the location of
+// the reference within root.
+func ListFragments(root dump.Root, write func(path, ref string)) {
+	for _, library := range root.Libraries {
+		p := path.Join("libraries", library.Name)
+		write(path.Join(p, "Summary"), library.Struct.Summary)
+		write(path.Join(p, "Description"), library.Struct.Description)
+		fragWriteFields(write, path.Join(p, "Fields"), library.Struct.Fields)
+		fragWriteTypes(write, path.Join(p, "Types"), library.Types)
 	}
-	buf.WriteString("\n")
+	fragWriteTypes(write, "types", root.Types)
+	fragWriteFormats(write, "formats", root.Formats)
+	for _, frag := range root.Fragments {
+		write("fragments", frag)
+	}
 }
 
-func fragWriteFormats(buf *bufio.Writer, p string, formats dump.Formats) {
+func fragWriteFormats(write func(path, ref string), p string, formats dump.Formats) {
 	sortFormats(formats, func(formatName string, format dump.Format) {
 		p := path.Join(p, formatName)
-		fragWrite(buf, path.Join(p, "Summary"), format.Summary)
-		fragWrite(buf, path.Join(p, "Description"), format.Description)
+		write(path.Join(p, "Summary"), format.Summary)
+		write(path.Join(p, "Description"), format.Description)
 	})
 }
 
-func fragWriteFields(buf *bufio.Writer, p string, fields dump.Fields) {
+func fragWriteFields(write func(path, ref string), p string, fields dump.Fields) {
 	sortFields(fields, func(fieldName string, field dump.Value) {
 		p := path.Join(p, fieldName)
 		switch value := field.(type) {
 		case dump.Property:
-			fragWrite(buf, path.Join(p, "Summary"), value.Summary)
-			fragWrite(buf, path.Join(p, "Description"), value.Description)
+			write(path.Join(p, "Summary"), value.Summary)
+			write(path.Join(p, "Description"), value.Description)
 		case dump.Struct:
-			fragWrite(buf, path.Join(p, "Summary"), value.Summary)
-			fragWrite(buf, path.Join(p, "Description"), value.Description)
-			fragWriteFields(buf, p, value.Fields)
+			write(path.Join(p, "Summary"), value.Summary)
+			write(path.Join(p, "Description"), value.Description)
+			fragWriteFields(write, p, value.Fields)
 		case dump.Function:
-			fragWrite(buf, path.Join(p, "Summary"), value.Summary)
-			fragWrite(buf, path.Join(p, "Description"), value.Description)
+			write(path.Join(p, "Summary"), value.Summary)
+			write(path.Join(p, "Description"), value.Description)
 		case dump.MultiFunction:
 			for i, value := range value {
-				fragWrite(buf, path.Join(p, strconv.Itoa(i), "Summary"), value.Summary)
-				fragWrite(buf, path.Join(p, strconv.Itoa(i), "Description"), value.Description)
+				write(path.Join(p, strconv.Itoa(i), "Summary"), value.Summary)
+				write(path.Join(p, strconv.Itoa(i), "Description"), value.Description)
 			}
 		}
 	})
 }
 
-func fragWriteTypes(buf *bufio.Writer, p string, types dump.TypeDefs) {
+func fragWriteTypes(write func(path, ref string), p string, types dump.TypeDefs) {
 	sortTypeDefs(types, func(defName string, def dump.TypeDef) {
 		p := path.Join(p, defName)
-		fragWrite(buf, path.Join(p, "Summary"), def.Summary)
-		fragWrite(buf, path.Join(p, "Description"), def.Description)
+		write(path.Join(p, "Summary"), def.Summary)
+		write(path.Join(p, "Description"), def.Description)
 		sortConstructors(def.Constructors, func(ctorName string, ctor dump.MultiFunction) {
 			p := path.Join(p, "Constructors", ctorName)
 			for i, fn := range ctor {
 				p := path.Join(p, strconv.Itoa(i))
-				fragWrite(buf, path.Join(p, "Summary"), fn.Summary)
-				fragWrite(buf, path.Join(p, "Description"), fn.Description)
+				write(path.Join(p, "Summary"), fn.Summary)
+				write(path.Join(p, "Description"), fn.Description)
 			}
 		})
 		sortProperties(def.Properties, func(propName string, prop dump.Property) {
 			p := path.Join(p, "Properties", propName)
-			fragWrite(buf, path.Join(p, "Summary"), prop.Summary)
-			fragWrite(buf, path.Join(p, "Description"), prop.Description)
+			write(path.Join(p, "Summary"), prop.Summary)
+			write(path.Join(p, "Description"), prop.Description)
 		})
 		sortProperties(def.Symbols, func(symName string, prop dump.Property) {
 			p := path.Join(p, "Symbols", symName)
-			fragWrite(buf, path.Join(p, "Summary"), prop.Summary)
-			fragWrite(buf, path.Join(p, "Description"), prop.Description)
+			write(path.Join(p, "Summary"), prop.Summary)
+			write(path.Join(p, "Description"), prop.Description)
 		})
 		sortMethods(def.Methods, func(methodName string, method dump.Function) {
 			p := path.Join(p, "Methods", methodName)
-			fragWrite(buf, path.Join(p, "Summary"), method.Summary)
-			fragWrite(buf, path.Join(p, "Description"), method.Description)
+			write(path.Join(p, "Summary"), method.Summary)
+			write(path.Join(p, "Description"), method.Description)
 		})
 		if op := def.Operators; op != nil {
 			p := path.Join(p, "Operators")
 			for i, fn := range op.Add {
-				fragWrite(buf, path.Join(p, "Add", strconv.Itoa(i), "Summary"), fn.Summary)
-				fragWrite(buf, path.Join(p, "Add", strconv.Itoa(i), "Description"), fn.Description)
+				write(path.Join(p, "Add", strconv.Itoa(i), "Summary"), fn.Summary)
+				write(path.Join(p, "Add", strconv.Itoa(i), "Description"), fn.Description)
 			}
 			for i, fn := range op.Sub {
-				fragWrite(buf, path.Join(p, "Sub", strconv.Itoa(i), "Summary"), fn.Summary)
-				fragWrite(buf, path.Join(p, "Sub", strconv.Itoa(i), "Description"), fn.Description)
+				write(path.Join(p, "Sub", strconv.Itoa(i), "Summary"), fn.Summary)
+				write(path.Join(p, "Sub", strconv.Itoa(i), "Description"), fn.Description)
 			}
 			for i, fn := range op.Mul {
-				fragWrite(buf, path.Join(p, "Mul", strconv.Itoa(i), "Summary"), fn.Summary)
-				fragWrite(buf, path.Join(p, "Mul", strconv.Itoa(i), "Description"), fn.Description)
+				write(path.Join(p, "Mul", strconv.Itoa(i), "Summary"), fn.Summary)
+				write(path.Join(p, "Mul", strconv.Itoa(i), "Description"), fn.Description)
 			}
 			for i, fn := range op.Div {
-				fragWrite(buf, path.Join(p, "Div", strconv.Itoa(i), "Summary"), fn.Summary)
-				fragWrite(buf, path.Join(p, "Div", strconv.Itoa(i), "Description"), fn.Description)
+				write(path.Join(p, "Div", strconv.Itoa(i), "Summary"), fn.Summary)
+				write(path.Join(p, "Div", strconv.Itoa(i), "Description"), fn.Description)
 			}
 			for i, fn := range op.Mod {
-				fragWrite(buf, path.Join(p, "Mod", strconv.Itoa(i), "Summary"), fn.Summary)
-				fragWrite(buf, path.Join(p, "Mod", strconv.Itoa(i), "Description"), fn.Description)
+				write(path.Join(p, "Mod", strconv.Itoa(i), "Summary"), fn.Summary)
+				write(path.Join(p, "Mod", strconv.Itoa(i), "Description"), fn.Description)
 			}
 			for i, fn := range op.Pow {
-				fragWrite(buf, path.Join(p, "Pow", strconv.Itoa(i), "Summary"), fn.Summary)
-				fragWrite(buf, path.Join(p, "Pow", strconv.Itoa(i), "Description"), fn.Description)
+				write(path.Join(p, "Pow", strconv.Itoa(i), "Summary"), fn.Summary)
+				write(path.Join(p, "Pow", strconv.Itoa(i), "Description"), fn.Description)
 			}
 			for i, fn := range op.Concat {
-				fragWrite(buf, path.Join(p, "Concat", strconv.Itoa(i), "Summary"), fn.Summary)
-				fragWrite(buf, path.Join(p, "Concat", strconv.Itoa(i), "Description"), fn.Description)
+				write(path.Join(p, "Concat", strconv.Itoa(i), "Summary"), fn.Summary)
+				write(path.Join(p, "Concat", strconv.Itoa(i), "Description"), fn.Description)
 			}
 			if op.Eq != nil {
-				fragWrite(buf, path.Join(p, "Eq", "Summary"), op.Eq.Summary)
-				fragWrite(buf, path.Join(p, "Eq", "Description"), op.Eq.Description)
+				write(path.Join(p, "Eq", "Summary"), op.Eq.Summary)
+				write(path.Join(p, "Eq", "Description"), op.Eq.Description)
 			}
 			if op.Lt != nil {
-				fragWrite(buf, path.Join(p, "Lt", "Summary"), op.Lt.Summary)
-				fragWrite(buf, path.Join(p, "Lt", "Description"), op.Lt.Description)
+				write(path.Join(p, "Lt", "Summary"), op.Lt.Summary)
+				write(path.Join(p, "Lt", "Description"), op.Lt.Description)
 			}
 			if op.Le != nil {
-				fragWrite(buf, path.Join(p, "Le", "Summary"), op.Le.Summary)
-				fragWrite(buf, path.Join(p, "Le", "Description"), op.Le.Description)
+				write(path.Join(p, "Le", "Summary"), op.Le.Summary)
+				write(path.Join(p, "Le", "Description"), op.Le.Description)
 			}
 			if op.Len != nil {
-				fragWrite(buf, path.Join(p, "Len", "Summary"), op.Len.Summary)
-				fragWrite(buf, path.Join(p, "Len", "Description"), op.Len.Description)
+				write(path.Join(p, "Len", "Summary"), op.Len.Summary)
+				write(path.Join(p, "Len", "Description"), op.Len.Description)
 			}
 			if op.Unm != nil {
-				fragWrite(buf, path.Join(p, "Unm", "Summary"), op.Unm.Summary)
-				fragWrite(buf, path.Join(p, "Unm", "Description"), op.Unm.Description)
+				write(path.Join(p, "Unm", "Summary"), op.Unm.Summary)
+				write(path.Join(p, "Unm", "Description"), op.Unm.Description)
 			}
 			if op.Call != nil {
-				fragWrite(buf, path.Join(p, "Call", "Summary"), op.Call.Summary)
-				fragWrite(buf, path.Join(p, "Call", "Description"), op.Call.Description)
+				write(path.Join(p, "Call", "Summary"), op.Call.Summary)
+				write(path.Join(p, "Call", "Description"), op.Call.Description)
 			}
 			if op.Index != nil {
-				fragWrite(buf, path.Join(p, "Index", "Summary"), op.Index.Summary)
-				fragWrite(buf, path.Join(p, "Index", "Description"), op.Index.Description)
+				write(path.Join(p, "Index", "Summary"), op.Index.Summary)
+				write(path.Join(p, "Index", "Description"), op.Index.Description)
 			}
 			if op.Newindex != nil {
-				fragWrite(buf, path.Join(p, "Newindex", "Summary"), op.Newindex.Summary)
-				fragWrite(buf, path.Join(p, "Newindex", "Description"), op.Newindex.Description)
+				write(path.Join(p, "Newindex", "Summary"), op.Newindex.Summary)
+				write(path.Join(p, "Newindex", "Description"), op.Newindex.Description)
 			}
 		}
 	})
