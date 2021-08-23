1e39d809c5b09e08e059acd3e862a52ce0dcde72
Anaminus <anaminus@protonmail.com>
2021-08-22 22:18:05 -0500

    Fix PullEncodedFromDict not checking when EncodingTypes is empty.

--- world_reflect.go
+++ world_reflect.go
@@ -238,7 +238,7 @@ func (w *World) PullEncodedFormat(lv lua.LValue, f Format) (v types.Value, err e
 // PullEncodedFromDict is like PullEncoded, but the value is pulled from a
 // Dictionary.
 func (w *World) PullEncodedFromDict(table *lua.LTable, field string, format rtypes.FormatSelector) (v types.Value, err error) {
-	if format := w.Format(format.Format); format.Name != "" {
+	if format := w.Format(format.Format); format.Name != "" && len(format.EncodeTypes) > 0 {
 		return w.PullAnyFromDictionaryOpt(table, field, nil, format.EncodeTypes...)
 	}
 	return w.PullFromDictionaryOpt(table, field, nil, "Variant")
