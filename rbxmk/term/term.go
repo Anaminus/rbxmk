2a32b39be42f911ca91d0d50b09d2190e5ef7d1a
Anaminus <anaminus@protonmail.com>
2022-03-05 15:06:43 -0600

    Begin implementing terminal renderer.
    Text is facilitated through a writer that handles the formatting. When
    this writer is flushed, the accumulated content is divided into
    paragraphs. Each paragraph is trimmed, unwrapped, then wrapped to a
    specified width. An optional correction is applied to account for edge
    cases with terminal wrapping. Paragraphs are then written with
    consistent gaps between.
    
    All text nodes are rendered except for those between sections. Code
    spans include delimiting ` characters. Paragraphs are ensured to have
    at least two newlines before and after (more than two are collapsed by
    the writer).

--- rbxmk/term/term.go
+++ rbxmk/term/term.go
@@ -2,12 +2,16 @@
 package term
 
 import (
+	"bytes"
+	"errors"
 	"io"
+	"unicode"
 
 	"github.com/PuerkitoBio/goquery"
+	"golang.org/x/net/html"
 )
 
-// Renderer implements a goldmark Renderer by rendering a stripped down format
+// Renderer implements a rendering HTML content in a stripped down format
 // suitable for the terminal.
 type Renderer struct {
 	// Width sets the assumed width of the terminal, which aids in wrapping
@@ -19,6 +23,214 @@ type Renderer struct {
 	TabSize int
 }
 
+var (
+	stop         = errors.New("stop")          // Stop walking without erroring.
+	skipChildren = errors.New("skip children") // Skip all child nodes.
+	skipText     = errors.New("skip text")     // Skip child text nodes.
+)
+
+type walker func(node *html.Node, entering bool) error
+
+func walk(node *html.Node, cb walker) error {
+	err := cb(node, true)
+	if err != nil && err != skipChildren && err != skipText {
+		return err
+	}
+	if err != skipChildren {
+		for c := node.FirstChild; c != nil; c = c.NextSibling {
+			if c.Type == html.TextNode && err == skipText {
+				continue
+			}
+			if err := walk(c, cb); err != nil {
+				return err
+			}
+		}
+	}
+	if err = cb(node, false); err != nil && err != skipChildren && err != skipText {
+		return err
+	}
+	return err
+}
+
+// Facilitates wrapped text.
+type writer struct {
+	bytes.Buffer
+	w io.Writer
+
+	// If wrapping, wrap to this width.
+	width int
+
+	// Size of tab character.
+	tabSize int
+
+	// If false, remove newlines that are inserted just after the edge of the
+	// width. This prevents wrapping terminals from incorrectly producing an
+	// extra gap. This removes the newline entirely, so it is assumed that the
+	// wrapping will separate the previous word will be separated from the next
+	// word.
+	//
+	// The gap fix is applied only if width is greater than 0.
+	noGapFix bool
+}
+
+func (w *writer) Flush() error {
+	b := w.Buffer.Bytes()
+	defer w.Buffer.Reset()
+
+	b = bytes.TrimSpace(b)
+	var paragraphs [][]rune
+	for i := 0; i < len(b); {
+		if b[i] == '\n' {
+			// Bounds check unneeded; if b[i], being a \n, was at the end, it
+			// would have been trimmed.
+			if b[i+1] == '\n' {
+				// New paragraph.
+				paragraphs = append(paragraphs, []rune(string(b[:i])))
+				i += 2
+				for b[i] == '\n' {
+					// Collapse extra newlines.
+					i++
+				}
+				b = b[i:]
+				i = 0
+				continue
+			} else {
+				// Unwrap.
+				b[i] = ' '
+			}
+		}
+		i++
+	}
+	if w.width > 0 {
+		for j, p := range paragraphs {
+			for i := 0; i < len(p); {
+				n := i + w.width
+				if n+1 >= len(p) {
+					break
+				}
+				for n > i && !unicode.IsSpace(p[n]) {
+					n--
+				}
+				if n <= i {
+					// Long word.
+					n = i + w.width
+					p = append(p, 0)
+					copy(p[n+1:], p[n:])
+					p[n] = '\n'
+					i = n + 1
+				} else {
+					p[n] = '\n'
+					i = n + 1
+				}
+			}
+			paragraphs[j] = p
+		}
+		if !w.noGapFix {
+			for i, p := range paragraphs {
+				for h, i := 0, 0; i < len(p); i++ {
+					if p[i] != '\n' {
+						continue
+					}
+					if i-h >= w.width {
+						copy(p[i:], p[i+1:])
+						p = p[:len(p)-1]
+						h = i
+						i--
+					} else {
+						h = i + 1
+					}
+				}
+				paragraphs[i] = p
+			}
+		}
+	}
+	for i, p := range paragraphs {
+		if i > 0 {
+			w.w.Write([]byte{'\n', '\n'})
+		}
+		if _, err := w.w.Write([]byte(string(p))); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
 func (r Renderer) Render(w io.Writer, s *goquery.Selection) error {
-	return goquery.Render(w, s)
+	buf := &writer{
+		w:       w,
+		width:   r.Width,
+		tabSize: r.TabSize,
+	}
+	for _, node := range s.Nodes {
+		if node.Type == html.TextNode {
+			continue
+		}
+		err := walk(node, func(node *html.Node, entering bool) error {
+			switch node.Type {
+			case html.ErrorNode:
+			case html.TextNode:
+				if entering {
+					buf.WriteString(node.Data)
+				}
+			case html.DocumentNode:
+			case html.ElementNode:
+				h := handlers[elementMatcher{node.Data, entering}]
+				if h != nil {
+					return h(buf, node)
+				}
+			case html.CommentNode:
+			case html.DoctypeNode:
+			case html.RawNode:
+			}
+			return nil
+		})
+		if err != nil && err != stop {
+			return err
+		}
+	}
+	return buf.Flush()
+}
+
+type elementMatcher struct {
+	data     string
+	entering bool
+}
+
+type nodeHandlers map[elementMatcher]func(w *writer, node *html.Node) error
+
+func isElement(node *html.Node, tag string) bool {
+	return node.Type == html.ElementNode && node.Data == tag
+}
+
+var handlers = nodeHandlers{
+	{"code", true}: func(w *writer, node *html.Node) error {
+		if isElement(node.Parent, "pre") { // May have syntax: `class="language-*"`
+			// Block
+			w.WriteString(node.FirstChild.Data)
+			return skipChildren
+		} else {
+			// Inline
+			return w.WriteByte('`')
+		}
+	},
+	{"code", false}: func(w *writer, node *html.Node) error {
+		if isElement(node.Parent, "pre") {
+			// Block
+		} else {
+			// Inline
+			return w.WriteByte('`')
+		}
+		return nil
+	},
+	{"section", true}: func(w *writer, node *html.Node) error {
+		return skipText
+	},
+	{"p", true}: func(w *writer, node *html.Node) error {
+		_, err := w.WriteString("\n\n")
+		return err
+	},
+	{"p", false}: func(w *writer, node *html.Node) error {
+		_, err := w.WriteString("\n\n")
+		return err
+	},
 }
