26cb014a6d2419cb16f1a63cbb0182d457d94330
Anaminus <anaminus@protonmail.com>
2022-03-06 00:11:48 -0600

    Override command usage template.

--- rbxmk/main.go
+++ rbxmk/main.go
@@ -8,12 +8,41 @@ import (
 	"github.com/anaminus/cobra"
 )
 
+const usageTemplate = `Usage:{{if .Runnable}}
+  {{.UseLine}}{{end}}{{if .HasAvailableSubCommands}}
+  {{.CommandPath}} [command]{{end}}{{if gt (len .Aliases) 0}}
+
+Aliases:
+  {{.NameAndAliases}}{{end}}{{if .HasExample}}
+
+Examples:
+{{.Example}}{{end}}{{if .HasAvailableSubCommands}}
+
+Available Commands:{{range .Commands}}{{if (or .IsAvailableCommand (eq .Name "help"))}}
+  {{rpad .Name .NamePadding }} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableLocalFlags}}
+
+Flags:
+{{.LocalFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasAvailableInheritedFlags}}
+
+Global Flags:
+{{.InheritedFlags.FlagUsages | trimTrailingWhitespaces}}{{end}}{{if .HasHelpSubCommands}}
+
+Additional help topics:{{range .Commands}}{{if .IsAdditionalHelpTopicCommand}}
+  {{rpad .CommandPath .CommandPathPadding}} {{.Short}}{{end}}{{end}}{{end}}{{if .HasAvailableSubCommands}}
+
+Use "{{.CommandPath}} [command] --help" for more information about a command.{{end}}
+`
+
 var Program = &cobra.Command{
 	Use:           "rbxmk",
 	Short:         Doc("Commands:Summary"),
 	Long:          Doc("Commands:Summary"),
 	SilenceUsage:  false,
 	SilenceErrors: true,
+}
+
+func init() {
+	Program.SetUsageTemplate(usageTemplate)
 }
 
 func main() {
