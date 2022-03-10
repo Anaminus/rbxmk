f73cec5358ea66424d263d9216c0325f9969a971
Anaminus <anaminus@protonmail.com>
2022-03-09 19:36:27 -0600

    Add example that downloads a list of audio assets.

---
+++ doc/examples/download-audio.rbxmk.lua
@@ -0,0 +1,34 @@
+-- Download list of audio assets. Input is a file containing a list of asset
+-- IDs, one per line. Output is a path to a directory. Assumes mp3 format.
+--
+-- Usage:
+--     rbxmk run download-audio.rbxmk.lua INPUT OUTPUT
+--
+-- Example:
+--     rbxmk run download-audio.rbxmk.lua assetList.txt assets
+
+local assetIDs, outputDirectory = ...
+fs.mkdir(outputDirectory, true)
+
+local list = fs.read(assetIDs, "bin")
+for id in string.gmatch(list, "[^\r\n]+") do
+	id = tonumber(id)
+	if id then
+		local ok, data = pcall(rbxassetid.read, {
+			AssetID = id,
+			Format = "bin",
+		})
+		if not ok then
+			print(string.format("download %10d: %s", id, data))
+		else
+			-- WARNING: May not actually be mp3.
+			local file = path.join(outputDirectory, id .. ".mp3")
+			local ok, err = pcall(fs.write, file, data, "bin")
+			if not ok then
+				print(string.format("write    %10d: %s", id, err))
+			else
+				print(string.format("ok       %10d", id))
+			end
+		end
+	end
+end
