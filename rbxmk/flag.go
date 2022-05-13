377bf5ad94e35176e9c17cf8298c2adbfc1350ca
Anaminus <anaminus@protonmail.com>
2022-05-13 10:27:50 -0500

    Remove unused repeatedString type.

--- rbxmk/flag.go
+++ rbxmk/flag.go
@@ -1,21 +1,5 @@
 package main
 
-import (
-	"strings"
-)
-
-// repeatedString is a string flag that can be specified multiple times.
-type repeatedString []string
-
-func (s repeatedString) String() string {
-	return strings.Join(s, ",")
-}
-
-func (s *repeatedString) Set(v string) error {
-	*s = append(*s, v)
-	return nil
-}
-
 type funcFlag func(string) error
 
 func (f funcFlag) Set(s string) error { return f(s) }
